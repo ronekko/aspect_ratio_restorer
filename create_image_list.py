6ea8ee9e9253920351dad78b65d9adc3ad7b41e2
Yamane Sasuke <is0240he@ed.ritsumei.ac.jp>
2016-11-10 21:03:07 +0900

    チャンネル数3の画像のみパスを取得する

--- create_image_list.py
+++ create_image_list.py
@@ -6,21 +6,20 @@ Created on Tue Oct 18 20:24:10 2016
 """
 
 import os
-import numpy as np
-from skimage import color, io
+from skimage import io
 import tqdm
 
 
 if __name__ == '__main__':
-    data_location = r'E:\dataset\Places_205'
-    dataset_root_dir = r'images'
-    root_dir_path = os.path.join(data_location, dataset_root_dir)
+    data_location = r'E:\stanford_Dogs_Dataset\Images'
     f = open("file_list.txt", "w")
-    for root, dirs, files in os.walk(root_dir_path):
+    for root, dirs, files in os.walk(data_location):
         for file_name in tqdm.tqdm(files):
             file_path = os.path.join(root, file_name)
             image = io.imread(file_path)
             if len(image.shape) == 2:
+                continue
+            elif len(image[0][0]) != 3:
                 continue
             f.write(file_path + "\n")
     f.close()
