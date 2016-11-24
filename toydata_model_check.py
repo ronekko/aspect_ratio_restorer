e958ff9381888897872448ee7154facf6ee5b7f0
Yamane Sasuke <is0240he@ed.ritsumei.ac.jp>
2016-11-24 20:42:26 +0900

    各層の出力チャンネル数を可視化した

--- toydata_model_check.py
+++ toydata_model_check.py
@@ -5,36 +5,43 @@ Created on Thu Nov 24 01:26:22 2016
 @author: yamane
 """
 
+import toydata
 import numpy as np
-from chainer import cuda, optimizers, Chain, serializers
+from chainer import cuda, optimizers, Chain
 import chainer.functions as F
 import chainer.links as L
 import matplotlib.pyplot as plt
-from skimage import io, transform, draw
 import time
 import copy
 import tqdm
-import cv2
 
 
 # ネットワークの定義
 class Convnet(Chain):
-    def __init__(self):
-        super(Convnet, self).__init__(
-            conv1=L.Convolution2D(1, 8, 3, stride=2, pad=1),
-            norm1=L.BatchNormalization(8),
-            conv2=L.Convolution2D(8, 8, 3, stride=2, pad=1),
-            norm2=L.BatchNormalization(8),
-            conv3=L.Convolution2D(8, 16, 3, stride=2, pad=1),
-            norm3=L.BatchNormalization(16),
-            conv4=L.Convolution2D(16, 16, 3, stride=2, pad=1),
-            norm4=L.BatchNormalization(16),
-            conv5=L.Convolution2D(16, 32, 3, stride=2, pad=1),
-            norm5=L.BatchNormalization(32),
+    def __init__(self, conv1_out_channel=4, conv2_out_channel=4,
+                 conv3_out_channel=8, conv4_out_channel=8,
+                 conv5_out_channel=16):
+        self.oc1 = conv1_out_channel
+        self.oc2 = conv2_out_channel
+        self.oc3 = conv3_out_channel
+        self.oc4 = conv4_out_channel
+        self.oc5 = conv5_out_channel
 
-            l1=L.Linear(1568, 500),
-            norm7=L.BatchNormalization(500),
-            l2=L.Linear(500, 1),
+        super(Convnet, self).__init__(
+            conv1=L.Convolution2D(1, self.oc1, 3, stride=2, pad=1),
+            norm1=L.BatchNormalization(self.oc1),
+            conv2=L.Convolution2D(self.oc1, self.oc2, 3, stride=2, pad=1),
+            norm2=L.BatchNormalization(self.oc2),
+            conv3=L.Convolution2D(self.oc2, self.oc3, 3, stride=2, pad=1),
+            norm3=L.BatchNormalization(self.oc3),
+            conv4=L.Convolution2D(self.oc3, self.oc4, 3, stride=2, pad=1),
+            norm4=L.BatchNormalization(self.oc4),
+            conv5=L.Convolution2D(self.oc4, self.oc5, 3, stride=2, pad=1),
+            norm5=L.BatchNormalization(self.oc5),
+
+            l1=L.Linear(784, 300),
+            norml=L.BatchNormalization(300),
+            l2=L.Linear(300, 1),
         )
 
     def network(self, X, test):
@@ -43,7 +50,7 @@ class Convnet(Chain):
         h = F.relu(self.norm3(self.conv3(h), test=test))
         h = F.relu(self.norm4(self.conv4(h), test=test))
         h = F.relu(self.norm5(self.conv5(h), test=test))
-        h = F.relu(self.norm7(self.l1(h), test=test))
+        h = F.relu(self.norml(self.l1(h), test=test))
         y = self.l2(h)
         return y
 
@@ -70,186 +77,14 @@ class Convnet(Chain):
             accuracies.append(cuda.to_cpu(accuracy.data))
         return np.mean(losses), np.mean(accuracies)
 
-
-class RandomCircleSquareDataset(object):
-    def __init__(self, image_size=500, circle_r_min=50, circle_r_max=150,
-                 size_min=50, size_max=200, p=[0.3, 0.3, 0.4], output_size=224,
-                 aspect_ratio_max=4, aspect_ratio_min=1):
-        self.image_size = image_size
-        self.cr_min = circle_r_min
-        self.cr_max = circle_r_max
-        self.size_min = size_min
-        self.size_max = size_max
-        self.p = p
-        self.output_size = output_size
-        self.ar_max = aspect_ratio_max
-        self.ar_min = aspect_ratio_min
-
-    def read_images_and_T(self, batch_size):
-        images = []
-        ts = []
-
-        for i in range(batch_size):
-            image = self.create_image()
-            t = np.random.choice(2)
-            if t == 1:
-                r = sample_random_aspect_ratio(self.ar_max, self.ar_min)
-            else:
-                r = 1
-            image = change_aspect_ratio(image, r)
-            square_image = padding_image(image)
-            resize_image = cv2.resize(
-                square_image, (self.output_size, self.output_size))
-            resize_image = resize_image[..., None]
-            images.append(resize_image)
-            ts.append(t)
-        X = np.stack(images, axis=0)
-        X = np.transpose(X, (0, 3, 1, 2))
-        X = X.astype(np.float32)
-        T = np.array(ts, dtype=np.int32).reshape(-1, 1)
-
-        return X, T
-
-    def create_image(self):
-        case = np.random.choice(3, p=self.p)
-        if case == 0:
-            image = self.create_random_circle(
-                self.image_size, self.cr_min, self.cr_max)
-        elif case == 1:
-            image = self.create_random_square(
-                self.image_size, self.size_min, self.size_max)
-        else:
-            image = self.create_random_circle_square(
-                self.image_size, self.cr_min, self.cr_max,
-                self.size_min, self.size_max)
-
-        return image
-
-    def create_random_circle(self, image_size, r_min, r_max):
-        image = np.zeros((image_size, image_size), dtype=np.float64)
-        r = np.random.randint(r_min, r_max)
-        x = np.random.randint(r-1, image_size - r + 1)
-        y = np.random.randint(r-1, image_size - r + 1)
-
-        rr, cc = draw.circle(x, y, r)
-        image[rr, cc] = 1
-
-        image = np.reshape(image, (image_size, image_size, 1))
-        return image
-
-    def create_random_square(self, image_size, size_min, size_max):
-        image = np.zeros((image_size, image_size), dtype=np.float64)
-        size = np.random.randint(size_min, size_max)
-        x = np.random.randint(0, image_size-size+1)
-        y = np.random.randint(0, image_size-size+1)
-
-        for i in range(0, size):
-            rr, cr = draw.line(y, x+i, y+size-1, x+i)
-            image[rr, cr] = 1
-
-        image = np.reshape(image, (image_size, image_size, 1))
-        return image
-
-    def create_random_circle_square(
-            self, image_size, r_min, r_max, size_min, size_max):
-        circle = self.create_random_circle(image_size, r_min, r_max)
-        square = self.create_random_square(image_size, size_min, size_max)
-        image = np.logical_or(circle, square)
-        image = image.astype(np.float64)
-        return image
-
     def __repr__(self):
-        template = """image_size:{}
-circle_min:{}
-circle_max:{}
-size_min:{}
-size_max:{}
-p:{}
-output_size:{}
-aspect_ratio_min:{}
-aspect_ratio_max:{}"""
-        return template.format(self.image_size, self.cr_min, self.cr_max,
-                               self.size_min, self.size_max, self.p,
-                               self.output_size, self.ar_min, self.ar_max)
+        template = """conv1_out_channel:{}
+conv2_out_channel:{}
+conv3_out_channel:{}
+conv4_out_channel:{}
+conv5_out_channel:{}"""
+        return template.format(self.oc1, self.oc2, self.oc3, self.oc4, self.oc5)
 
-
-def change_aspect_ratio(image, aspect_ratio):
-    h_image, w_image = image.shape[:2]
-    r = aspect_ratio
-
-    if r == 1:
-        return image
-    elif r > 1:
-        w_image = int(w_image * r)
-    else:
-        h_image = int(h_image / float(r))
-    # cv2.resizeは引数が（image, (w, h))の順
-    # transform.resizeは引数が(image, (h, w))の順
-    resize_image = cv2.resize(image, (w_image, h_image))[..., None]
-    return resize_image
-
-
-def crop_center(image):
-    height, width = image.shape[:2]
-    left = 0
-    right = width
-    top = 0
-    bottom = height
-
-    if height >= width:  # 縦長の場合
-        output_size = width
-        margin = int((height - width) / 2)
-        top = margin
-        bottom = top + output_size
-    else:  # 横長の場合
-        output_size = height
-        margin = int((width - height) / 2)
-        left = margin
-        right = left + output_size
-
-    square_image = image[top:bottom, left:right]
-
-    return square_image
-
-
-def padding_image(image):
-    height, width = image.shape[:2]
-    left = 0
-    right = width
-    top = 0
-    bottom = height
-
-    if height >= width:  # 縦長の場合
-        output_size = height
-        margin = int((height - width) / 2)
-        left = margin
-        right = left + width
-    else:  # 横長の場合
-        output_size = width
-        margin = int((width - height) / 2)
-        top = margin
-        bottom = top + height
-
-    square_image = np.zeros((output_size, output_size, 1))
-    square_image[top:bottom, left:right] = image
-
-    return square_image
-
-
-def transpose(image):
-    if image.ndim == 2:
-        image = image.T
-    else:
-        image = np.transpose(image, (1, 0, 2))
-    return image
-
-
-def sample_random_aspect_ratio(r_max, r_min=1):
-    # アスペクト比rをランダム生成する
-    r = np.random.uniform(r_min, r_max)
-    if np.random.rand() > 0.5:
-        r = 1 / r
-    return r
 
 if __name__ == '__main__':
     # 超パラメータ
@@ -269,7 +104,7 @@ if __name__ == '__main__':
     aspect_ratio_min = 2
 
     model = Convnet().to_gpu()
-    dataset = RandomCircleSquareDataset(
+    dataset = toydata.RandomCircleSquareDataset(
         image_size, circle_r_min, circle_r_max, size_min, size_max, p,
         output_size, aspect_ratio_max, aspect_ratio_min)
     # Optimizerの設定
@@ -292,7 +127,8 @@ if __name__ == '__main__':
             losses = []
             accuracies = []
             for i in tqdm.tqdm(range(num_batches)):
-                X_batch, T_batch = dataset.read_images_and_T(batch_size)
+                X_batch, T_batch = dataset.minibatch_binary_classification(
+                    batch_size)
                 X_batch = cuda.to_gpu(X_batch)
                 T_batch = cuda.to_gpu(T_batch)
                 # 勾配を初期化
@@ -311,7 +147,8 @@ if __name__ == '__main__':
             epoch_loss.append(np.mean(losses))
             epoch_accuracy.append(np.mean(accuracies))
 
-            X_valid, T_valid = dataset.read_images_and_T(num_valid)
+            X_valid, T_valid = dataset.minibatch_binary_classification(
+                num_valid)
             loss_valid, accuracy_valid = model.loss_ave(
                 X_valid, T_valid, batch_size, True)
 
@@ -324,6 +161,7 @@ if __name__ == '__main__':
                 model_best = copy.deepcopy(model)
 
             # 訓練データでの結果を表示
+            print "toydata_model_check"
             print "epoch:", epoch
             print "time", epoch_time, "(", total_time, ")"
             print "loss[train]:", epoch_loss[epoch]
@@ -334,6 +172,7 @@ if __name__ == '__main__':
 
             plt.plot(epoch_loss)
             plt.plot(epoch_valid_loss)
+            plt.ylim(0, 1)
             plt.title("loss")
             plt.legend(["train", "valid"], loc="upper right")
             plt.grid()
@@ -358,3 +197,4 @@ if __name__ == '__main__':
     print 'train_size', num_train
     print 'valid_size', num_valid
     print dataset
+    print model
