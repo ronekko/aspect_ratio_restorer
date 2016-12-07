7a47f75c30bf0e6df89c62e3034f4eda9f510e5c
Yamane Sasuke <is0240he@ed.ritsumei.ac.jp>
2016-12-07 16:08:52 +0900

    不要な変数を削除した

--- toydata_analyze.py
+++ toydata_analyze.py
@@ -13,10 +13,13 @@ from chainer import cuda, serializers, Variable
 
 
 def generate_image(model, X, T, max_iteration, a):
-    print 'T:', T[0], 'exp(T):', np.exp(T[0])
+    # 元のXを表示
+    print 'origin_T:', T[0], 'exp(origin_T):', np.exp(T[0])
     plt.matshow(X[0][0])
+    plt.title("origin_X")
     plt.colorbar()
     plt.show()
+
     X_data = Variable(cuda.to_gpu(X))
     for epoch in range(max_iteration):
         y = model.forward(X_data, True)
@@ -25,18 +28,16 @@ def generate_image(model, X, T, max_iteration, a):
         X_data = Variable(X_data.data + a * X_data.grad)
         X_new = cuda.to_cpu(X_data.data)
         X_new = X_new.reshape(-1, 224, 224)
-    print 'Y:', y.data[0], 'exp(Y):', cuda.cupy.exp(y.data[0])
+    # 最適化後のXを表示
+    print 'new_T:', y.data[0], 'exp(new_T):', cuda.cupy.exp(y.data[0])
     plt.matshow(X_new[0])
+    plt.title("new_X")
     plt.colorbar()
     plt.show()
 
 if __name__ == '__main__':
     # 超パラメータ
     max_iteration = 500  # 繰り返し回数
-    batch_size = 100  # ミニバッチサイズ
-    num_train = 5000
-    num_valid = 100
-    learning_rate = 0.001
     image_size = 500
     circle_r_min = 50
     circle_r_max = 150
@@ -54,30 +55,25 @@ if __name__ == '__main__':
     dataset = toydata.RandomCircleSquareDataset(
         image_size, circle_r_min, circle_r_max, size_min, size_max, p,
         output_size, aspect_ratio_max, aspect_ratio_min)
-
+    # テストデータを取得
     X, T = dataset.minibatch_regression(1)
-
-    # yを計算
-    X_gpu = Variable(cuda.to_gpu(X))
-    X_data = Variable(cuda.to_gpu(X))
-
+    # Rが大きくなるようにXを最適化する
     generate_image(model, X, T, max_iteration, step_size)
 
+    X_gpu = Variable(cuda.to_gpu(X))
+    # yを計算
     y = model.forward(X_gpu, True)
-
     # 特徴マップを取得
-    a = y.creator.inputs[0]
-    l = []
-    while a.creator:
-        if a.creator.label == 'Convolution2DFunction':
-            l.append(cuda.to_cpu(a.data))
-        a = a.creator.inputs[0]
-
-    # 特徴マップを表示
+#    a = y.creator.inputs[0]
+#    l = []
+#    while a.creator:
+#        if a.creator.label == 'Convolution2DFunction':
+#            l.append(cuda.to_cpu(a.data))
+#        a = a.creator.inputs[0]
+#    # 特徴マップを表示
 #    for f in l[-1][0]:
 #        plt.matshow(f, cmap=plt.cm.gray)
 #        plt.show()
-
     # 出力に対する入力の勾配を可視化
     y.grad = cuda.cupy.ones(y.data.shape, dtype=np.float32)
     y.backward(retain_grad=True)
