cdb4d3356365c925218d19025e1b725756a41344
Yamane Sasuke <is0240he@ed.ritsumei.ac.jp>
2016-12-07 15:49:39 +0900

    ｒが大きくなるように入力画像を最適化する関数を実装した

--- toydata_analyze.py
+++ toydata_analyze.py
@@ -8,22 +8,31 @@ Created on Thu Dec 01 04:13:19 2016
 import toydata
 import toydata_regression
 import numpy as np
-import time
-import tqdm
-import h5py
-import copy
 import matplotlib.pyplot as plt
-from multiprocessing import Process, Queue
-from chainer import cuda, optimizers, Chain, serializers,Variable
-import chainer.functions as F
-import chainer.links as L
-import cv2
-from dog_data_regression import Convnet
+from chainer import cuda, serializers, Variable
 
+
+def generate_image(model, X, T, max_iteration, a):
+    print 'T:', T[0], 'exp(T):', np.exp(T[0])
+    plt.matshow(X[0][0])
+    plt.colorbar()
+    plt.show()
+    X_data = Variable(cuda.to_gpu(X))
+    for epoch in range(max_iteration):
+        y = model.forward(X_data, True)
+        y.grad = cuda.cupy.ones(y.data.shape, dtype=np.float32)
+        y.backward(retain_grad=True)
+        X_data = Variable(X_data.data + a * X_data.grad)
+        X_new = cuda.to_cpu(X_data.data)
+        X_new = X_new.reshape(-1, 224, 224)
+    print 'Y:', y.data[0], 'exp(Y):', cuda.cupy.exp(y.data[0])
+    plt.matshow(X_new[0])
+    plt.colorbar()
+    plt.show()
 
 if __name__ == '__main__':
     # 超パラメータ
-    max_iteration = 100  # 繰り返し回数
+    max_iteration = 500  # 繰り返し回数
     batch_size = 100  # ミニバッチサイズ
     num_train = 5000
     num_valid = 100
@@ -37,9 +46,10 @@ if __name__ == '__main__':
     output_size = 224
     aspect_ratio_max = 4
     aspect_ratio_min = 2
+    step_size = 0.01
 
     model = toydata_regression.Convnet().to_gpu()
-    serializers.load_npz('model1480557475.95toydata2.npz',model)
+    serializers.load_npz('model1480565177.66toydata2.npz', model)
 
     dataset = toydata.RandomCircleSquareDataset(
         image_size, circle_r_min, circle_r_max, size_min, size_max, p,
@@ -49,6 +59,10 @@ if __name__ == '__main__':
 
     # yを計算
     X_gpu = Variable(cuda.to_gpu(X))
+    X_data = Variable(cuda.to_gpu(X))
+
+    generate_image(model, X, T, max_iteration, step_size)
+
     y = model.forward(X_gpu, True)
 
     # 特徴マップを取得
@@ -60,9 +74,9 @@ if __name__ == '__main__':
         a = a.creator.inputs[0]
 
     # 特徴マップを表示
-    for f in l[-1][0]:
-        plt.matshow(f, cmap=plt.cm.gray)
-        plt.show()
+#    for f in l[-1][0]:
+#        plt.matshow(f, cmap=plt.cm.gray)
+#        plt.show()
 
     # 出力に対する入力の勾配を可視化
     y.grad = cuda.cupy.ones(y.data.shape, dtype=np.float32)
