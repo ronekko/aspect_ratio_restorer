aafdc49eb767dbd387a6698b338d1c13b0d8fd74
Yamane Sasuke <is0240he@ed.ritsumei.ac.jp>
2016-12-08 15:15:16 +0900

    特徴マップの表示を関数化した

--- toydata_analyze.py
+++ toydata_analyze.py
@@ -7,6 +7,7 @@ Created on Thu Dec 01 04:13:19 2016
 
 import numpy as np
 import matplotlib.pyplot as plt
+import cv2
 from chainer import cuda, serializers, Variable
 import toydata
 import toydata_regression
@@ -25,23 +26,70 @@ def generate_image(model, X, T, max_iteration, a):
         X_new = X_new.reshape(-1, 224, 224)
         print 'origin_T:', T[0], 'exp(origin_T):', np.exp(T[0])
         print 'new_T:', y.data[0], 'exp(new_T):', cuda.cupy.exp(y.data[0])
-        # 元のXを表示
+    # 元のXを表示
 #        print 'origin_T:', T[0], 'exp(origin_T):', np.exp(T[0])
-        plt.matshow(X[0][0], cmap=plt.cm.gray)
-        plt.title("origin_X")
-        plt.colorbar()
-        plt.show()
-        # 最適化後のXを表示
+    plt.matshow(X[0][0], cmap=plt.cm.gray)
+    plt.title("origin_X")
+    plt.colorbar()
+    plt.show()
+    # 最適化後のXを表示
 #        print 'new_T:', y.data[0], 'exp(new_T):', cuda.cupy.exp(y.data[0])
-        plt.matshow(X_new[0], cmap=plt.cm.gray)
-        plt.title("new_X")
-        plt.colorbar()
-        plt.show()
+    plt.matshow(X_new[0], cmap=plt.cm.gray)
+    plt.title("new_X")
+    plt.colorbar()
+    plt.show()
     return X_new
+
+
+def get_receptive_field(y):
+    # 特徴マップを取得
+    a = y.creator.inputs[0]
+    l = []
+    while a.creator:
+        if a.creator.label == 'ReLU':
+            l.append(cuda.to_cpu(a.data))
+        a = a.creator.inputs[0]
+    return l
+
+
+def check_use_channel(l, layer):
+    use_channel = []
+    layer = len(l) - layer
+    for c in range(l[layer].shape[1:2][0]):
+        temp = []
+        for b in range(batch_size):
+            temp.append(np.sum(l[layer][b][c]))
+        ave = np.average(temp)
+        use_channel.append(ave)
+    return use_channel
+
+
+def minibatch_regression(dataset, batch_size, r):
+    images = []
+    ts = []
+
+    for i in range(batch_size):
+        image = dataset.create_image()
+        image = toydata.change_aspect_ratio(image, r)
+        square_image = toydata.padding_image(image)
+        # cv2.resize:(image, (w, h))
+        # transform.resize:(image, (h, w))
+        resize_image = cv2.resize(
+            square_image, (dataset.output_size, dataset.output_size))
+        resize_image = resize_image[..., None]
+        images.append(resize_image)
+        t = np.log(r)
+        ts.append(t)
+    X = np.stack(images, axis=0)
+    X = np.transpose(X, (0, 3, 1, 2))
+    X = X.astype(np.float32)
+    T = np.array(ts, dtype=np.float32).reshape(-1, 1)
+    return X, T
 
 if __name__ == '__main__':
     # 超パラメータ
-    max_iteration = 100000  # 繰り返し回数
+    max_iteration = 500  # 繰り返し回数
+    batch_size = 1
     image_size = 500
     circle_r_min = 50
     circle_r_max = 150
@@ -51,45 +99,97 @@ if __name__ == '__main__':
     output_size = 224
     aspect_ratio_max = 2
     aspect_ratio_min = 2
-    step_size = 0.00001
+    step_size = 0.0001
 
 #    model = toydata_regression.Convnet().to_gpu()
 #    serializers.load_npz('toydata_regression_min2.npz', model)
-    model = toydata_regression_ave_pooling.Convnet().to_gpu()
-    serializers.load_npz('toydata_regression_avepooling_min2.npz', model)
+    model = toydata_regression.Convnet().to_gpu()
+    serializers.load_npz('model1481172416.48toydata2.npz', model)
 
     dataset = toydata.RandomCircleSquareDataset(
         image_size, circle_r_min, circle_r_max, size_min, size_max, p,
         output_size, aspect_ratio_max, aspect_ratio_min)
     # テストデータを取得
-    X, T = dataset.minibatch_regression(1)
+    X_yoko, T_yoko = minibatch_regression(dataset, batch_size, 2)
+    X_tate, T_tate = minibatch_regression(dataset, batch_size, 0.5)
     # Rが大きくなるようにXを最適化する
-    X_new = generate_image(model, X, T, max_iteration, step_size)
+    X_new = generate_image(model, X_yoko, T_yoko, max_iteration, step_size)
 
-    X_gpu = Variable(cuda.to_gpu(X))
+    X_yoko_gpu = Variable(cuda.to_gpu(X_yoko))
+    X_tate_gpu = Variable(cuda.to_gpu(X_tate))
     # yを計算
-    y = model.forward(X_gpu, True)
+    y_yoko = model.forward(X_yoko_gpu, True)
+    y_tate = model.forward(X_tate_gpu, True)
     # 特徴マップを取得
-#    a = y.creator.inputs[0]
-#    l = []
-#    while a.creator:
-#        if a.creator.label == 'Convolution2DFunction':
-#            l.append(cuda.to_cpu(a.data))
-#        a = a.creator.inputs[0]
-#    # 特徴マップを表示
-#    for f in l[-1][0]:
+    l_yoko = get_receptive_field(y_yoko)
+    l_tate = get_receptive_field(y_tate)
+    # 特徴マップを表示
+#    for f in l_yoko[-1][0]:
 #        plt.matshow(f, cmap=plt.cm.gray)
 #        plt.show()
+#    for f in l_tate[-1][0]:
+#        plt.matshow(f, cmap=plt.cm.gray)
+#        plt.show()
+    l5_yoko = check_use_channel(l_yoko, 5)
+    l4_yoko = check_use_channel(l_yoko, 4)
+    l3_yoko = check_use_channel(l_yoko, 3)
+    l2_yoko = check_use_channel(l_yoko, 2)
+    l1_yoko = check_use_channel(l_yoko, 1)
+    l5_tate = check_use_channel(l_tate, 5)
+    l4_tate = check_use_channel(l_tate, 4)
+    l3_tate = check_use_channel(l_tate, 3)
+    l2_tate = check_use_channel(l_tate, 2)
+    l1_tate = check_use_channel(l_tate, 1)
+
+    plt.plot(l1_yoko)
+    plt.plot(l1_tate)
+    plt.title("layer1")
+    plt.legend(["yoko", "tate"], loc="lower right")
+    plt.show()
+    plt.plot(l2_yoko)
+    plt.plot(l2_tate)
+    plt.title("layer2")
+    plt.legend(["yoko", "tate"], loc="lower right")
+    plt.show()
+    plt.plot(l3_yoko)
+    plt.plot(l3_tate)
+    plt.title("layer3")
+    plt.legend(["yoko", "tate"], loc="lower right")
+    plt.show()
+    plt.plot(l4_yoko)
+    plt.plot(l4_tate)
+    plt.title("layer4")
+    plt.legend(["yoko", "tate"], loc="lower right")
+    plt.show()
+    plt.plot(l5_yoko)
+    plt.plot(l5_tate)
+    plt.title("layer5")
+    plt.legend(["yoko", "tate"], loc="lower right")
+    plt.show()
     # 出力に対する入力の勾配を可視化
-    y.grad = cuda.cupy.ones(y.data.shape, dtype=np.float32)
-    y.backward(retain_grad=True)
-    grad = X_gpu.grad
+    y_yoko.grad = cuda.cupy.ones(y_yoko.data.shape, dtype=np.float32)
+    y_yoko.backward(retain_grad=True)
+    grad = X_yoko_gpu.grad
     grad = cuda.to_cpu(grad)
     for c in grad[0]:
         plt.matshow(c, cmap=plt.cm.bwr)
+        plt.title("yoko")
         plt.colorbar()
         plt.show()
-#    for c in X[0]:
-#        plt.matshow(c, cmap=plt.cm.gray)
-#        plt.colorbar()
-#        plt.show()
+    y_tate.grad = cuda.cupy.ones(y_tate.data.shape, dtype=np.float32)
+    y_tate.backward(retain_grad=True)
+    grad = X_tate_gpu.grad
+    grad = cuda.to_cpu(grad)
+    for c in grad[0]:
+        plt.matshow(c, cmap=plt.cm.bwr)
+        plt.title("tate")
+        plt.colorbar()
+        plt.show()
+    for c in X_yoko[0]:
+        plt.matshow(c, cmap=plt.cm.gray)
+        plt.colorbar()
+        plt.show()
+    for c in X_tate[0]:
+        plt.matshow(c, cmap=plt.cm.gray)
+        plt.colorbar()
+        plt.show()
