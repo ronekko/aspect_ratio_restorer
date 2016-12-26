8dc4976fa3953389f4ec653f47bdf030685f244e
Yamane Sasuke <is0240he@ed.ritsumei.ac.jp>
2016-12-26 14:51:34 +0900

    モジュールに対応

--- dogdata_analyze.py
+++ dogdata_analyze.py
@@ -8,40 +8,14 @@ Created on Fri Oct 21 16:12:33 2016
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-import cv2
-import h5py
 from multiprocessing import Process, Queue
+
 from chainer import cuda, serializers, Variable
-import toydata
+
 import dog_data_regression
 import dog_data_regression_ave_pooling
 import dog_data_regression_max_pooling
-import gray2rgb
-import toydata_analyze
-
-
-def output(model, X, T):
-    predict_t = model.predict(X, True)
-    target_t = T
-    predict_r = np.exp(predict_t)
-    target_r = np.exp(target_t)
-    predict_image = toydata.fix_image(X, predict_r)
-    original_image = toydata.fix_image(X, target_r)
-    debased_image = np.transpose(X[0], (1, 2, 0))
-    predict_image = np.transpose(predict_image, (1, 2, 0))
-    original_image = np.transpose(original_image, (1, 2, 0))
-    print 'predict t:', predict_t, 'target t:', target_t
-    print 'predict r:', predict_r, 'target r:', target_r
-    plt.subplot(131)
-    plt.title("debased_image")
-    plt.imshow(debased_image/256.0)
-    plt.subplot(132)
-    plt.title("fix_image")
-    plt.imshow(predict_image/256.0)
-    plt.subplot(133)
-    plt.title("target_image")
-    plt.imshow(original_image/256.0)
-    plt.show()
+import load_datasets
 
 
 def generate_image(model, X, T, max_iteration, a):
@@ -96,127 +70,93 @@ def check_use_channel(l, layer):
     return use_channel
 
 
-def minibatch_regression(file_path, data, batch_size, r, crop_size=224,
-                         output_size=256):
-    dataset = h5py.File(file_path)
-    image_features = dataset['image_features']
-
-    num_batches = len(data) / batch_size
-
-    for indexes in np.array_split(data, num_batches):
-        images = []
-        ts = []
-        image_batch = image_features[indexes.tolist()]
-        for i in range(len(indexes)):
-            image = image_batch[i]
-            image = toydata.change_aspect_ratio(image, r)
-            square_image = toydata.crop_center(image)
-            resize_image = cv2.resize(square_image,
-                                      (output_size, output_size))
-            resize_image = toydata.random_crop_and_flip(resize_image,
-                                                        crop_size)
-            images.append(resize_image)
-            t = np.log(r)
-            ts.append(t)
-        X = np.stack(images, axis=0)
-        X = np.transpose(X, (0, 3, 1, 2))
-        X = X.astype(np.float32)
-        T = np.array(ts, dtype=np.float32).reshape(-1, 1)
-        return X, T
-
-
 if __name__ == '__main__':
     file_name = os.path.splitext(os.path.basename(__file__))[0]
     # 超パラメータ
-    max_iteration = 1000  # 繰り返し回数
-    batch_size = 1
-    num_train = 0
-    num_test = 1
-    output_size = 256
-    crop_size = 224
-    aspect_ratio_max = 3
-    aspect_ratio_min = 1.0
-    step_size = 0.1
-    file_path = r'E:\voc2012\raw_dataset\output_size_500\output_size_500.hdf5'
-    model_file = 'model_dog_reg1481285466.8.npz'
+    max_iteration = 150  # 繰り返し回数
+    batch_size = 100  # ミニバッチサイズ
+    num_train = 20000  # 学習データ数
+    num_test = 500  # 検証データ数
+    output_size = 256  # 生成画像サイズ
+    crop_size = 224  # ネットワーク入力画像サイズ
+    aspect_ratio_min = 1.0  # 最小アスペクト比の誤り
+    aspect_ratio_max = 1.5  # 最大アスペクト比の誤り
+    step_size = 0.01
+    test_ratio = [1/2.0, 1/1.9, 1/1.8, 1/1.7, 1/1.6, 1/1.5, 1/1.4, 1/1.3, 1/1.2, 1/1.1, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0]
+    crop = True
+    test = True
+    hdf5_filepath = r'E:\stanford_Dogs_Dataset\raw_dataset_binary\output_size_500\output_size_500.hdf5'  # データセットファイル保存場所
+    model_file = r'C:\Users\yamane\Dropbox\correct_aspect_ratio\dog_data_regression_ave_pooling\1482373541.87_asp_max_2.0\dog_data_regression_ave_pooling1482373541.87.npz'
+    t_losses = []
+    t_max = 0
+    t_min = np.inf
+    # バッチサイズ計算
+    train_data = range(0, num_train)
     test_data = range(num_train, num_train + num_test)
-
-    model = dog_data_regression.Convnet().to_gpu()
+    num_batches_train = num_train / batch_size
+    num_batches_test = num_test / batch_size
+    # stream作成
+    dog_stream_train, dog_stream_test = load_datasets.load_dog_stream(
+        hdf5_filepath, batch_size)
+    # モデル読み込み
+    model = dog_data_regression_ave_pooling.Convnet().to_gpu()
+    # Optimizerの設定
     serializers.load_npz(model_file, model)
 
-    queue_test = Queue(1)
-    process_test = Process(target=dog_data_regression.create_mini_batch,
-                           args=(queue_test, file_path, test_data,
-                                 1, aspect_ratio_min, aspect_ratio_max,
-                                 crop_size, output_size))
-    process_test.start()
-
-#    # テスト用のデータを取得
-    X_test, T_test = queue_test.get()
-    # rを指定してデータを取得
-#    X_yoko, T_yoko = minibatch_regression(file_path, test_data, batch_size, 2)
-#    X_tate, T_tate = minibatch_regression(file_path, test_data, batch_size, 0.5)
-    # 人工画像を取得
-#    X_test, T_test = gray2rgb.create_minibatch(1)
-#    X_test = gray2rgb.change_rgb_minibatch(X_test)
-
-#    # 復元結果を表示
-    output(model, X_test, T_test)
-
-    # Rが大きくなるようにXを最適化する
-#    X_new = generate_image(model, X_test, T_test, max_iteration, step_size)
-
-    X_test_gpu = Variable(cuda.to_gpu(X_test))
-#    X_tate_gpu = Variable(cuda.to_gpu(X_tate))
+    try:
+        for r in test_ratio:
+            queue = Queue(10)
+            process = Process(target=load_datasets.load_data,
+                              args=(queue, dog_stream_test, crop,
+                                    aspect_ratio_max, aspect_ratio_min,
+                                    output_size, crop_size, test, r))
+            process.start()
+            t_loss = []
+            for i in range(num_batches_test):
+                # テスト用のデータを取得
+                X_test, T_test = queue.get()
+                # 復元結果を表示
+                t_loss = dog_data_regression.test_output(model, X_test, T_test,
+                                                         t_loss)
+            t_losses.append(np.mean(t_loss))
+            process.terminate()
+    except KeyboardInterrupt:
+        print "割り込み停止が実行されました"
+    plt.plot(np.log(test_ratio), t_losses)
+    plt.title("t_losses")
+    plt.xlim(np.log(0.5), np.log(2.0))
+    plt.grid()
+    plt.show()
+#    X_test_gpu = Variable(cuda.to_gpu(X_test))
 #    # yを計算
-    y_test = model.forward(X_test_gpu, True)
-#    y_tate = model.forward(X_tate_gpu, True)
+#    y_test = model.forward(X_test_gpu, True)
 #    # 特徴マップを取得
-#    l_yoko = get_receptive_field(y_yoko)
-#    l_tate = get_receptive_field(y_tate)
+#    l = get_receptive_field(y_test)
 #    # 特徴マップを表示
-#    for f in l_yoko[-1][0]:
+#    for f in l[-1][0]:
 #        plt.matshow(f, cmap=plt.cm.gray)
 #        plt.show()
-#    for f in l_tate[-1][0]:
-#        plt.matshow(f, cmap=plt.cm.gray)
-#        plt.show()
-#    # 特徴マップの使用率を取得
-#    l5_yoko = check_use_channel(l_yoko, 5)
-#    l4_yoko = check_use_channel(l_yoko, 4)
-#    l3_yoko = check_use_channel(l_yoko, 3)
-#    l2_yoko = check_use_channel(l_yoko, 2)
-#    l1_yoko = check_use_channel(l_yoko, 1)
-#    l5_tate = check_use_channel(l_tate, 5)
-#    l4_tate = check_use_channel(l_tate, 4)
-#    l3_tate = check_use_channel(l_tate, 3)
-#    l2_tate = check_use_channel(l_tate, 2)
-#    l1_tate = check_use_channel(l_tate, 1)
-#    # 特徴マップの使用率を表示
-#    plt.plot(l1_yoko)
-#    plt.plot(l1_tate)
+    # 特徴マップの使用率を取得
+#    l5 = check_use_channel(l, 5)
+#    l4 = check_use_channel(l, 4)
+#    l3 = check_use_channel(l, 3)
+#    l2 = check_use_channel(l, 2)
+#    l1 = check_use_channel(l, 1)
+    # 特徴マップの使用率を表示
+#    plt.plot(l1)
 #    plt.title("layer1")
-#    plt.legend(["yoko", "tate"], loc="lower right")
 #    plt.show()
-#    plt.plot(l2_yoko)
-#    plt.plot(l2_tate)
+#    plt.plot(l2)
 #    plt.title("layer2")
-#    plt.legend(["yoko", "tate"], loc="lower right")
 #    plt.show()
-#    plt.plot(l3_yoko)
-#    plt.plot(l3_tate)
+#    plt.plot(l3)
 #    plt.title("layer3")
-#    plt.legend(["yoko", "tate"], loc="lower right")
 #    plt.show()
-#    plt.plot(l4_yoko)
-#    plt.plot(l4_tate)
+#    plt.plot(l4)
 #    plt.title("layer4")
-#    plt.legend(["yoko", "tate"], loc="lower right")
 #    plt.show()
-#    plt.plot(l5_yoko)
-#    plt.plot(l5_tate)
+#    plt.plot(l5)
 #    plt.title("layer5")
-#    plt.legend(["yoko", "tate"], loc="lower right")
 #    plt.show()
     # 出力に対する入力の勾配を可視化
 #    y_test.grad = cuda.cupy.ones(y_test.data.shape, dtype=np.float32)
@@ -224,35 +164,18 @@ if __name__ == '__main__':
 #    grad = X_test_gpu.grad
 #    grad = cuda.to_cpu(grad)
 #    for c in grad[0]:
-#        plt.imshow(c, vmin=0.035, vmax=-0.035, cmap=plt.cm.bwr)
-##        plt.title("yoko")
+#        plt.imshow(c/256.0, vmin=0.035, vmax=-0.035, cmap=plt.cm.bwr)
 #        plt.colorbar()
 #        plt.show()
-#    y_tate.grad = cuda.cupy.ones(y_tate.data.shape, dtype=np.float32)
-#    y_tate.backward(retain_grad=True)
-#    grad = X_tate_gpu.grad
-#    grad = cuda.to_cpu(grad)
-#    for c in grad[0]:
-#        plt.matshow(c, cmap=plt.cm.bwr)
-#        plt.title("tate")
-#        plt.colorbar()
-#        plt.show()
-#     入力画像を表示
-    for c in X_test:
-        c = np.transpose(c, (1, 2, 0))
-        plt.imshow(c/256.0, cmap=plt.cm.gray)
-#        plt.colorbar()
-        plt.show()
-#    for c in X_tate[0]:
-#        plt.matshow(c, cmap=plt.cm.gray)
+    # 入力画像を表示
+#    for c in X_test[0:1]:
+#        c = np.transpose(c, (1, 2, 0))
+#        plt.imshow(c/256.0, cmap=plt.cm.gray)
 #        plt.colorbar()
 #        plt.show()
 
-    process_test.terminate()
-    print 'max_iteration', max_iteration
-    print 'batch_size', batch_size
-    print 'output_size', output_size
-    print 'aspect_ratio_max', aspect_ratio_max
-    print 'aspect_ratio_min', aspect_ratio_min
+    print 'test', test
+    print 'num_test', num_test
+    print 'r', r
     print 'step_size', step_size
     print 'model_file', model_file
