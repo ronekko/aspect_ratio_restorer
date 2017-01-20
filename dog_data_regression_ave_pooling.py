94e84953f93525486faab061c03fca8f52999682
Yamane Sasuke <is0240he@ed.ritsumei.ac.jp>
2017-01-20 21:08:11 +0900

    モデルを変更

--- dog_data_regression_ave_pooling.py
+++ dog_data_regression_ave_pooling.py
@@ -35,6 +35,8 @@ class Convnet(Chain):
             norm4=L.BatchNormalization(32),
             conv5=L.Convolution2D(32, 64, 3, stride=2, pad=1),
             norm5=L.BatchNormalization(64),
+            conv6=L.Convolution2D(64, 64, 1),
+            norm6=L.BatchNormalization(64),
 
             l1=L.Linear(64, 1)
         )
@@ -45,6 +47,7 @@ class Convnet(Chain):
         h = F.relu(self.norm3(self.conv3(h), test=test))
         h = F.relu(self.norm4(self.conv4(h), test=test))
         h = F.relu(self.norm5(self.conv5(h), test=test))
+        h = F.relu(self.norm6(self.conv6(h), test=test))
         h = F.average_pooling_2d(h, 7)
         y = self.l1(h)
         return y
